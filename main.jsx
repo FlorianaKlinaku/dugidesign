284bfa69a624625cb324aad6ecb91d8bb493cd5f
FlorianaKlinaku <florianaklinaku1@outlook.de>
2025-05-25 17:04:55 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const App = () => <h1>DugiDesign app is working!</h1>;
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
